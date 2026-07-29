010e9bee6a34d1ff3b44155e3763a518fde77c45
RipatAlam <mdalamm5090@gmail.com>
2026-07-29 19:44:09 +0300

    Assignment-server-m10-2026

--- src/app/(dashboardLayout)/dashboardfile/layout.jsx
+++ src/app/(dashboardLayout)/dashboardfile/layout.jsx
@@ -4,17 +4,13 @@ import React from "react";
 const DashboardLayout = ({ children }) => {
   return (
     <div className="flex min-h-screen bg-[#F8F4EE]">
-
       {/* SIDEBAR */}
       <DashboardLayoutSidebar />
-
-      {/* CONTENT */}
       <main className="flex-1 min-w-0">
         <div className="max-w-7xl mx-auto ml-70 p-4 sm:p-6 lg:p-8">
           {children}
         </div>
       </main>
-
     </div>
   );
 };
